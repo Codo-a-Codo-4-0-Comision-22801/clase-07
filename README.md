18b03e72a3e03062cb54db56855c589151a33702
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-09-15 20:34:58 -0300

    Clase 07 final + ejercicio

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # clase-07
+
+## Ejercicio Clase 07
+
+* Repetir lo de la clase 06
+* Van a crear los objetos(React.component) de la clase 05     
+    - Video
+    - Audio
+    - Boton
+    - Lista de elementos
---
+++ clase-07/src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './Button.css';
+
+export default class Button extends Component{
+    render(){
+        return(
+            <div>
+                <label>Hello there </label>
+                <button className="Button"> Hello Button World from React!</button>
+            </div>
+        );
+    }
+}
